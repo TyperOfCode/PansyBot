fbcd1786e8a8768358345c2e4dfe8c46720873b2
BMan <bmanrl@hotmail.com>
2020-01-04 00:57:22 -0400

    removed the incomplete rules commands from here. Gave them their own dedicated cog.

--- cogs/admin.py
+++ cogs/admin.py
@@ -29,23 +29,6 @@ class Admin(commands.Cog):
         else:
             return False
 
-    @commands.group(name="rule")
-    async def _rule(self, ctx):
-        if ctx.invoked_subcommand is None:
-            await ctx.author.send("Which rule would you like?\n\nEx: **p^rule** <number>")
-
-    @_rule.command(name="1")
-    async def _one(self, ctx, member: discord.Member = None):
-        embed = discord.Embed(title="Language", color=0xff00ff, description="The only language you're allowed to use is English, alongside common Romaji terms/phrases.\n\nE.g. Konichiwa")
-        embed.set_author(name="Rule 1")
-        embed.set_thumbnail(url=ctx.guild.icon_url)
-        await ctx.message.delete()
-        if member is None:
-            pass
-        else:
-            await ctx.send(member.mention, delete_after=30)
-        await ctx.send(embed=embed, delete_after=30)
-
     @commands.command(name="clear")
     @commands.is_owner()
     async def _clear(self, ctx, number: int = None, member: discord.Member = None):
