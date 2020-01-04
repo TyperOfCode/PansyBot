d56f27a98670a47204649ce0873d1c54e05eeddb
BMan <bmanrl@hotmail.com>
2020-01-04 12:36:03 -0400

    Removed VIP related messages.

--- cogs/bkgrnd.py
+++ cogs/bkgrnd.py
@@ -2,7 +2,7 @@ import discord
 import random
 import secrets
 import gspread
-import datetime
+from datetime import datetime
 from oauth2client.service_account import ServiceAccountCredentials
 from gspread_formatting import *
 from discord.utils import get
@@ -15,29 +15,28 @@ class bkgrnd(commands.Cog):
         self.counting.start()
         self.wordsplay.start()
         self.wyr.start()
-        self.vip.start()
-        self.network.start()
 
     def cog_unload(self):
         self.sheetcolor.cancel()
         self.counting.cancel()
         self.wordsplay.cancel()
         self.wyr.cancel()
-        self.vip.cancel()
-        self.network.cancel()
 
     @tasks.loop(minutes=1)
     async def sheetcolor(self):
-        if datetime.datetime.now().strftime("%M") == "00":
+        if datetime.now().strftime("%M") == "00":
+            # CONNECTING TO SHEET
             sheet = connectSheet("test")
+            # CONNECTING TO SHEET
+
             # COLOR FORMAT DECLARATION
             fmt = cellFormat(backgroundColor=color(0.64, 0.76, 0.96))
             fmt2 = cellFormat(backgroundColor=color(0.71, 0.84, 0.66))
             # COLOR FORMAT DECLARATION
 
             # DATETIME OBJECT COLLECTION
-            hour = datetime.datetime.utcnow().strftime("%H:00")
-            day = datetime.datetime.utcnow().strftime("%A, %b %d")
+            hour = datetime.utcnow().strftime("%H:00")
+            day = datetime.utcnow().strftime("%A, %b %d")
             # DATETIME OBJECT COLLECTION
 
             # GOOGLE SHEET OPERATIONS
@@ -46,26 +45,31 @@ class bkgrnd(commands.Cog):
             row = daycell.row # Row for day cell
             column = cell.col # Column for hour cell
             day = rowcol_to_a1(row, 1) # Day cell still (This is for formatting)
-            hourCell1 = rowcol_to_a1(row, column) # 2 Cells beside each other.
-            hourCell2 = rowcol_to_a1(row, column+1) # To accomodate for the hour.
+            hourCell1 = rowcol_to_a1(row, column) # [2 Cells beside each other.
+            hourCell2 = rowcol_to_a1(row, column+1) # To accomodate for the hour.]
             colorRange = f"{hourCell1}:{hourCell2}"
             format_cell_ranges(sheet, [(f'A1:AW10', fmt), (day, fmt2), (colorRange, fmt2)])
             # GOOGLE SHEET OPERATIONS
 
-    @tasks.loop(hours=6)
-    async def network(self):
-        guild = self.bot.get_guild(540784184470274069)
-        netwrk = guild.get_channel(626749195318984704)
-        embed = discord.Embed(color=0x00ffff, description="You can buy <@&636574850026176532> with 6k Tomi, and get access to VIP Chat!\nDM <@287343423088885771> to buy it!")
-        await netwrk.send(embed=embed)
+            iterableHours = []
 
-    @tasks.loop(hours=12)
-    async def vip(self):
-        guild = self.bot.get_guild(540784184470274069)
-        vipchan = guild.get_channel(635780188021456896)
-        emoji = get(guild.emojis, name="malRH")
-        embed = discord.Embed(title="Hey cuties!", color=0x00ffff, description=f"Please don't share links for T5 or T6 cards outside this channel!\n\nEnjoy your time. {emoji}")
-        await vipchan.send(embed=embed)
+            # SCANNING FOR UPCOMING SHIFTS
+            for i in range(0, 6):
+                dateToSearch = (datetime.utcnow()+timedelta(hours=i)).strftime("%A, %b %d")
+                hourToSearch = (datetime.utcnow()+timedelta(hours=i)).strftime("%H:00")
+                dateCell = sheet.find(dateToSearch).row
+                hourCell = sheet.find(hourToSearch).col
+                iterableHours.append([dateCell, hourCell], [dateCell, hourCell+1])
+            await ctx.send(iterableHours)
+            # SCANNING FOR UPCOMING SHIFTS
+
+            # INSERTING NEW DAY FOR THE SCHEDULE
+            if datetime.utcnow().strftime("%H:%M") == "00:00":
+                insertedDay = (datetime.utcnow()+timedelta(days=6)).strftime("%A, %b %d")
+                sheet.insert_row(values=[insertedDay], index=2, value_input_option="RAW")
+            else:
+                pass
+            # INSERTING NEW DAY FOR THE SCHEDULE
 
     @tasks.loop(hours=1)
     async def counting(self):
